39d12c8f7e109681d89bb19ca970d4d6c4ae159d
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-19 23:37:44 +0300

    Handling Errors

--- 1)PythonSyntax.py
+++ 1)PythonSyntax.py
@@ -6,3 +6,9 @@ print("Hello, Alex!")
 
 print("### Strings ###")
 print("Hello, " + "Python!")
+
+print("### Handling Errors ###")
+# print("How do you make a hot dog stand?')
+print("How do you make a hot dog stand?")
+# print(You take away its chair!)
+print("You take away its chair!")
