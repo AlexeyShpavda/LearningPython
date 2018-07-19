0329aed322e8f0b24961afe9ca2bac40a92aff8e
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-19 23:40:46 +0300

    Variables

--- 1)PythonSyntax.py
+++ 1)PythonSyntax.py
@@ -12,3 +12,7 @@ print("### Handling Errors ###")
 print("How do you make a hot dog stand?")
 # print(You take away its chair!)
 print("You take away its chair!")
+
+print("### Variables ###")
+todays_date = "19.07.2018"
+print(todays_date)
