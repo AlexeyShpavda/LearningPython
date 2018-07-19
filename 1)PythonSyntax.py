976cc617f1ec3c182cd8083939a44aab47a61239
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-19 23:48:49 +0300

    Updating Variables

--- 1)PythonSyntax.py
+++ 1)PythonSyntax.py
@@ -28,3 +28,26 @@ happy_division = 540 / 45
 print(happy_division)
 sassy_combinations = 129 * 1345 + 120 / 6 - 12
 print(sassy_combinations)
+
+print("### Updating Variables ###")
+january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
+annual_rainfall = january_to_june_rainfall
+
+july_rainfall = 1.05
+annual_rainfall += july_rainfall
+
+august_rainfall = 4.91
+annual_rainfall += august_rainfall
+
+september_rainfall = 5.16
+annual_rainfall += september_rainfall
+
+october_rainfall = 7.20
+annual_rainfall += october_rainfall
+
+november_rainfall = 5.06
+annual_rainfall += november_rainfall
+
+december_rainfall = 4.06
+annual_rainfall += december_rainfall
+print(annual_rainfall)
