60c27df664b574f99d4d114b99ff0dcaa95f0e13
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-19 23:43:33 +0300

    Arithmetic

--- 1)PythonSyntax.py
+++ 1)PythonSyntax.py
@@ -16,3 +16,15 @@ print("You take away its chair!")
 print("### Variables ###")
 todays_date = "19.07.2018"
 print(todays_date)
+
+print("### Arithmetic ###")
+mirthful_addition = 12381 + 91817
+print(mirthful_addition)
+amazing_subtraction = 981 - 312
+print(amazing_subtraction)
+trippy_multiplication = 38 * 902
+print(trippy_multiplication)
+happy_division = 540 / 45
+print(happy_division)
+sassy_combinations = 129 * 1345 + 120 / 6 - 12
+print(sassy_combinations)
