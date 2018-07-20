f2ff110772a61196f37adff2ff9a13984aa1139d
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-20 12:32:03 +0300

    Practice

--- 2)StringsAndConsoleOutput.py
+++ 2)StringsAndConsoleOutput.py
@@ -2,3 +2,12 @@ print("### Strings ###")
 name = "Alex"
 age = "19"
 city = "Grodno"
+
+print("### Practice ###")
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"
+
+print(caesar)
+print(praline)
+print(viking)
