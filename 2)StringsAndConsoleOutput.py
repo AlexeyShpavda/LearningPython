111bbf446cef9e5587dce3dd5d9e0af522536790
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-20 12:54:06 +0300

    str()

--- 2)StringsAndConsoleOutput.py
+++ 2)StringsAndConsoleOutput.py
@@ -19,3 +19,15 @@ print('This isn\'t flying, this is falling with style!')
 print("### Access by Index ###")
 fifth_letter = "MONTY"[4]
 print(fifth_letter)
+
+print("### String methods ###")
+parrot = "Norwegian Blue"
+print(parrot)
+print(len(parrot))
+print(parrot.lower())
+print(parrot.upper())
+
+
+print("### str() ###")
+pi = 3.14
+print(str(pi))
