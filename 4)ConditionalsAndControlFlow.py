9540d0588dc77205b8265d5c5a2f0d72871be8bb
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-21 11:52:59 +0300

    Compare Closely!

---
+++ 4)ConditionalsAndControlFlow.py
@@ -0,0 +1,17 @@
+print("### Compare Closely! ###")
+# Assign True or False as appropriate on the lines below!
+# True if 17 < 328 or to False if it is not.
+bool_one = True
+print(bool_one)
+# True if 100 == (2 * 50) or to False otherwise.
+bool_two = True
+print(bool_two)
+# True if 19 <= 19 or to False if it is not.
+bool_three = True
+print(bool_three)
+# True if -22 >= -18 or to False if it is not.
+bool_four = False
+print(bool_four)
+# True if 99 != (98 + 1) or to False otherwise.
+bool_five = False
+print(bool_five)
