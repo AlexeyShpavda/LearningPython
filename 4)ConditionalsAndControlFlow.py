a22a2259a45f6a33f963ab2b7337531fcd68bb29
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-21 12:41:19 +0300

    Else Problems, I Feel Bad for You, Son...

--- 4)ConditionalsAndControlFlow.py
+++ 4)ConditionalsAndControlFlow.py
@@ -134,3 +134,18 @@ def using_control_again():
 
 print(using_control_once())
 print(using_control_again())
+
+print("### Else Problems, I Feel Bad for You, Son... ###")
+answer = "'Tis but a scratch!"
+
+def black_knight():
+    if answer == "'Tis but a scratch!":
+        return True
+    else:
+        return False
+
+def french_soldier():
+    if answer == "Go away, or I shall taunt you a second time!":
+        return True
+    else:
+        return False
