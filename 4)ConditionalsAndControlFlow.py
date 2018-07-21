4abb5905beaa575c08258c7f35927c161695b651
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-21 12:43:22 +0300

    I Got 99 Problems, But a Switch Ain't One

--- 4)ConditionalsAndControlFlow.py
+++ 4)ConditionalsAndControlFlow.py
@@ -149,3 +149,18 @@ def french_soldier():
         return True
     else:
         return False
+
+print("### I Got 99 Problems, But a Switch Ain't One ###")
+
+
+def greater_less_equal_5(answer):
+    if answer > 5:
+        return 1
+    elif answer < 5:
+        return -1
+    else:
+        return 0
+
+print(greater_less_equal_5(4))
+print(greater_less_equal_5(5))
+print(greater_less_equal_5(6))
