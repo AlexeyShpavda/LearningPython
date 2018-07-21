2b02e1eeb66d2f755477af2dfcfb7f08b14de22e
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-21 12:19:40 +0300

    Or

--- 4)ConditionalsAndControlFlow.py
+++ 4)ConditionalsAndControlFlow.py
@@ -63,3 +63,15 @@ bool_four = -(1 ** 2) < 2 ** 0 and 10 % 10 <= 20 - 10 * 2
 print(bool_four)
 bool_five = True and True
 print(bool_five)
+
+print("### Or ###")
+bool_one = 2**3 == 108 % 100 or 'Cleese' == 'King Arthur'
+print(bool_one)
+bool_two = True or False
+print(bool_two)
+bool_three = 100**0.5 >= 50 or False
+print(bool_three)
+bool_four = True or True
+print(bool_four)
+bool_five = 1**100 == 100**1 or 3 * 2 * 1 != 3 + 2 + 1
+print(bool_five)
