8f0d61f4fe1b6971e7463cd8981c9a22dd2bb60e
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-21 13:28:18 +0300

    Check Yourself... Some More

--- 5)PygLatin.py
+++ 5)PygLatin.py
@@ -5,7 +5,13 @@ print("### Input! ###")
 original = input("Enter a word: ")
 
 print("### Check Yourself! ###")
-if len(original) > 0:
+# if len(original) > 0:
+#   print(original)
+# else:
+#   print("empty")
+
+print("### Check Yourself... Some More ###")
+if len(original) > 0 and original.isalpha():
   print(original)
 else:
   print("empty")
