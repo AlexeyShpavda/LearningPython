070e93928e01510d9650c90368a583a3eb80e44a
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-21 12:45:49 +0300

    The Big If

--- 4)ConditionalsAndControlFlow.py
+++ 4)ConditionalsAndControlFlow.py
@@ -164,3 +164,23 @@ def greater_less_equal_5(answer):
 print(greater_less_equal_5(4))
 print(greater_less_equal_5(5))
 print(greater_less_equal_5(6))
+
+print("### The Big If ###")
+def grade_converter(grade):
+    if grade >= 90:
+        return "A"
+    elif grade >= 80:
+        return "B"
+    elif grade >= 70:
+        return "C"
+    elif grade >= 65:
+        return "D"
+    else:
+        return "F"
+
+# This should print an "A"
+print(grade_converter(92))
+# This should print a "C"
+print(grade_converter(70))
+# This should print an "F"
+print(grade_converter(61))
