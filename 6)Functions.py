f786a33925e600d3dc884c6bacef59d23e319033
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-22 23:30:37 +0300

    Call and Response

--- 6)Functions.py
+++ 6)Functions.py
@@ -4,3 +4,13 @@ def spam():
   print("Eggs!")
 
 spam()
+
+print("### Call and Response ###")
+def square(n):
+    """Returns the square of a number."""
+    squared = n ** 2
+    print
+    "%d squared is %d." % (n, squared)
+    return squared
+
+square(10)
