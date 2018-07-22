c4824e867da4c13b3912e68e3d19fbbc12ae9208
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-23 00:21:19 +0300

    Getting There

--- 7)TakingAVacation.py
+++ 7)TakingAVacation.py
@@ -8,3 +8,14 @@ bigger(1, 3)
 print("### Planning Your Trip ###")
 def hotel_cost(nights):
   return 140 * nights
+
+print("### Getting There ###")
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    return 475
