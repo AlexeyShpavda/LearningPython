769458bba486188b44f97a47830067307a7981eb
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-23 16:08:58 +0300

    More with 'for'

--- 8)PythonListsAndDictionaries.py
+++ 8)PythonListsAndDictionaries.py
@@ -63,3 +63,13 @@ my_list = [1, 9, 3, 8, 5, 7]
 
 for number in my_list:
     print(number * 2)
+
+print("### More with 'for' ###")
+start_list = [5, 3, 1, 2, 4]
+square_list = []
+
+for number in start_list:
+  square_list.append(number ** 2)
+square_list.sort()
+
+print(square_list)
