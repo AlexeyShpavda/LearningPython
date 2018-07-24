79d8f46910bcffd75f48b1bdb69b140bf18985b4
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-24 15:36:29 +0300

    Lists + Functions

--- 9)ADayAtTheSupermarket.py
+++ 9)ADayAtTheSupermarket.py
@@ -18,3 +18,14 @@ a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 for number in a:
   if number % 2 == 0:
     print(number)
+
+print("### Lists + Functions ###")
+def fizz_count(list):
+  count = 0
+  for item in list:
+    if item == "fizz":
+      count = count + 1
+  return count
+
+list = ["fizz", "fizz", "fizz" , "x", "y", "z"]
+print(fizz_count(list))
