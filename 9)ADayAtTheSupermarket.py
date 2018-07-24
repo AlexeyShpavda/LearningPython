cda1aa38b17118c7c458b738f3265b02f79d254e
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-24 15:26:11 +0300

    This is KEY!

--- 9)ADayAtTheSupermarket.py
+++ 9)ADayAtTheSupermarket.py
@@ -2,3 +2,13 @@ print("### BeFOR We Begin ###")
 names = ["Alex", "Mariah", "Martine", "Columbus"]
 for name in names:
   print(name)
+
+print("### This is KEY! ###")
+webster = {
+  "Aardvark" : "A star of a popular children's cartoon show.",
+  "Baa" : "The sound a goat makes.",
+  "Carpet": "Goes on the floor.",
+  "Dab": "A small amount."
+}
+for word in webster:
+  print(webster[word])
