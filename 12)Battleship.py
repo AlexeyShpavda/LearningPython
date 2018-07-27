41980170cdb665670525abeb0e7d3760805b182b
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-27 12:01:24 +0300

    Custom Print

--- 12)Battleship.py
+++ 12)Battleship.py
@@ -6,5 +6,12 @@ for i in range(5):
   board.append(['O'] * 5)
 
 print("### Check it Twice ###")
-for i in board:
-  print(i)
+# for i in board:
+#   print(i)
+
+print("### Custom Print ###")
+def print_board(board_in):
+    for row in board_in:
+        print(row)
+
+print_board(board)
