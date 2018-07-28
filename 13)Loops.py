01a94cdfce512b41fb051521bf7e2215e344416d
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-28 12:02:22 +0300

    While you're at it

--- 13)Loops.py
+++ 13)Loops.py
@@ -14,3 +14,10 @@ loop_condition = True
 while loop_condition:
   print("I am a loop")
   loop_condition = False
+
+print("### While you're at it ###")
+num = 1
+
+while num <= 10:
+  print(num ** 2)
+  num += 1
