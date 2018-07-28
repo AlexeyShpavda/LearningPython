1d5ff0d8df817a74521f36af91c9e8011cbb0794
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-28 13:20:07 +0300

    Multiple lists

--- 13)Loops.py
+++ 13)Loops.py
@@ -137,3 +137,10 @@ choices = ['pizza', 'pasta', 'salad', 'nachos']
 print('Your choices are:')
 for index, item in enumerate(choices):
   print(index + 1, item)
+
+print("### Multiple lists ###")
+list_a = [3, 9, 17, 15, 19]
+list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
+
+for a, b in zip(list_a, list_b):
+  print(max(a, b))
