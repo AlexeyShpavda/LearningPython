f63f77c4abf992bbd90af7004e214932bd24d4d1
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-30 14:14:54 +0300

    Try It!

--- 16)AdvancedTopicsInPython.py
+++ 16)AdvancedTopicsInPython.py
@@ -86,3 +86,7 @@ print(filter(lambda x: x % 3 == 0, my_list))
 print("### Lambda Syntax ###")
 languages = ["HTML", "CSS", "JavaScript", "Python"]
 print(filter(lambda x: x == "Python", languages))
+
+print("### Try It! ###")
+squares = [x ** 2 for x in range(1, 11)]
+print(filter(lambda x: x >= 30 and x <= 70, squares))
