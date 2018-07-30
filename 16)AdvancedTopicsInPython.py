8469d8e279725bb52b23167aef52346459dfd565
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-30 13:09:13 +0300

    keys() and values()

--- 16)AdvancedTopicsInPython.py
+++ 16)AdvancedTopicsInPython.py
@@ -6,3 +6,13 @@ my_dict = {
 }
 
 print(my_dict.items())
+
+print("### keys() and values() ###")
+my_dict = {
+  'name': 'Nick',
+  'age':  31,
+  'occupation': 'Dentist',
+}
+
+print(my_dict.keys())
+print(my_dict.values())
