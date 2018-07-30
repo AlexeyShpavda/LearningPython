ec1dbf46297a15d6f57a3b0f95e16ab2bf5dd6de
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-30 13:30:44 +0300

    Now You Try!

--- 16)AdvancedTopicsInPython.py
+++ 16)AdvancedTopicsInPython.py
@@ -37,3 +37,7 @@ print(doubles_by_3)
 
 even_squares = [x ** 2 for x in range(1, 12) if x % 2 == 0]
 print(even_squares)
+
+print("### Now You Try! ###")
+cubes_by_four = [x ** 3 for x in range(1, 11) if ((x ** 3) % 4) == 0]
+print(cubes_by_four)
