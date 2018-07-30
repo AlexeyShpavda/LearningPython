b41a7608dc9a6cea6326340f5c2e825e7a2741c9
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-30 14:20:22 +0300

    Comprehending Comprehensions

--- 16)AdvancedTopicsInPython.py
+++ 16)AdvancedTopicsInPython.py
@@ -99,3 +99,7 @@ movies = {
 }
 
 print(movies.items())
+
+print("### Comprehending Comprehensions ###")
+threes_and_fives = [x for x in range(1, 16) if x % 3 == 0 or x % 5 == 0]
+print(threes_and_fives)
