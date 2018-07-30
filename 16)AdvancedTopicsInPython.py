770e5876901a0bb3d0f1167894d8fa6853eac84b
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-30 13:56:03 +0300

    Practice Makes Perfect

--- 16)AdvancedTopicsInPython.py
+++ 16)AdvancedTopicsInPython.py
@@ -64,7 +64,17 @@ letters = ['A', 'B', 'C', 'D', 'E']
 print(letters[::-1])
 
 print("### Stride Length ###")
-to_one_hundred = range(101)
+to_one_hundred = [i for i in range(101)]
 
 backwards_by_tens = to_one_hundred[::-10]
 print(backwards_by_tens)
+
+print("### Practice Makes Perfect ###")
+to_21 = [i for i in range(1, 22)]
+print(to_21)
+
+odds = to_21[::2]
+print(odds)
+
+middle_third = to_21[7:14]
+print(middle_third)
