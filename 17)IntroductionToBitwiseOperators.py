3c3cdeb676af14ffc6d8b3fe849984914f2bb8cd
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-31 13:03:34 +0300

    The Man Behind the Bit Mask

--- 17)IntroductionToBitwiseOperators.py
+++ 17)IntroductionToBitwiseOperators.py
@@ -95,3 +95,12 @@ print(bin(2))
 print(bin(3))
 print(bin(42))
 print(bin(123))
+
+print("### The Man Behind the Bit Mask ###")
+def check_bit4(input):
+  mask = 0b1000
+  desired = input & mask
+  if desired > 0:
+    return "on"
+  else:
+    return "off"
