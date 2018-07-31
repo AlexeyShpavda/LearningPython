420d47c8b8a8f4cb8aca769fb5f01d214e3e1ca2
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-07-31 12:35:59 +0300

    I Can Count to 1100!

--- 17)IntroductionToBitwiseOperators.py
+++ 17)IntroductionToBitwiseOperators.py
@@ -17,3 +17,17 @@ print(0b111)  #7
 print("******")
 print(0b1 + 0b11)
 print(0b11 * 0b11)
+
+print("### I Can Count to 1100! ###")
+one = 0b1
+two = 0b10
+three = 0b11
+four = 0b100
+five = 0b101
+six = 0b110
+seven = 0b111
+eight = 0b1000
+nine = 0b1001
+ten = 0b1010
+eleven = 0b1011
+twelve = 0b1100
