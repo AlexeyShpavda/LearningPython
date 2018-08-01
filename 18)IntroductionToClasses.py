3f79f92b8df14424ec3df6511018c3468dbf9909
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-08-01 09:46:45 +0300

    More on __init__() and self

--- 18)IntroductionToClasses.py
+++ 18)IntroductionToClasses.py
@@ -13,10 +13,26 @@ print("### Let's Not Get Too Selfish ###")
 #     self.name = name
 
 print("### Instantiating Your First Object ###")
+# class Animal(object):
+#     def __init__(self, name):
+#         self.name = name
+#
+# zebra = Animal("Jeffrey")
+#
+# print(zebra.name)
+
+print("### More on __init__() and self ###")
 class Animal(object):
-    def __init__(self, name):
-        self.name = name
+  """Makes cute animals."""
+  def __init__(self, name, age, is_hungry):
+    self.name = name
+    self.age = age
+    self.is_hungry = is_hungry
 
-zebra = Animal("Jeffrey")
+zebra = Animal("Jeffrey", 2, True)
+giraffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
 
-print(zebra.name)
+print(zebra.name, zebra.age, zebra.is_hungry)
+print(giraffe.name, giraffe.age, giraffe.is_hungry)
+print(panda.name, panda.age, panda.is_hungry)
