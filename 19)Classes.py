30121303dca1b8dce016ceeb2d9cc334e35046cc
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-08-01 16:00:48 +0300

    Modifying member variables

--- 19)Classes.py
+++ 19)Classes.py
@@ -37,6 +37,22 @@ print("### Referring to member variables ###")
 # print(my_car.mpg)
 
 print("### Creating class methods ###")
+# class Car(object):
+#     condition = "new"
+#
+#     def __init__(self, model, color, mpg):
+#         self.model = model
+#         self.color = color
+#         self.mpg = mpg
+#
+#     def display_car(self):
+#         print("This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg)))
+#
+# my_car = Car("DeLorean", "silver", 88)
+#
+# my_car.display_car()
+
+print("### Modifying member variables ###")
 class Car(object):
     condition = "new"
 
@@ -48,6 +64,11 @@ class Car(object):
     def display_car(self):
         print("This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg)))
 
+    def drive_car(self):
+        self.condition = "used"
+
 my_car = Car("DeLorean", "silver", 88)
 
-my_car.display_car()
+print(my_car.condition)
+my_car.drive_car()
+print(my_car.condition)
