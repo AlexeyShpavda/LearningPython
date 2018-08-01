c453d0f90fa1c70a76931f6a92f3895c3a8b69cd
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-08-01 09:51:28 +0300

    Class Scope

--- 18)IntroductionToClasses.py
+++ 18)IntroductionToClasses.py
@@ -22,17 +22,33 @@ print("### Instantiating Your First Object ###")
 # print(zebra.name)
 
 print("### More on __init__() and self ###")
+# class Animal(object):
+#   """Makes cute animals."""
+#   def __init__(self, name, age, is_hungry):
+#     self.name = name
+#     self.age = age
+#     self.is_hungry = is_hungry
+#
+# zebra = Animal("Jeffrey", 2, True)
+# giraffe = Animal("Bruce", 1, False)
+# panda = Animal("Chad", 7, True)
+#
+# print(zebra.name, zebra.age, zebra.is_hungry)
+# print(giraffe.name, giraffe.age, giraffe.is_hungry)
+# print(panda.name, panda.age, panda.is_hungry)
+
+print("### Class Scope ###")
 class Animal(object):
   """Makes cute animals."""
-  def __init__(self, name, age, is_hungry):
+  is_alive = True
+  def __init__(self, name, age):
     self.name = name
     self.age = age
-    self.is_hungry = is_hungry
 
-zebra = Animal("Jeffrey", 2, True)
-giraffe = Animal("Bruce", 1, False)
-panda = Animal("Chad", 7, True)
+zebra = Animal("Jeffrey", 2)
+giraffe = Animal("Bruce", 1)
+panda = Animal("Chad", 7)
 
-print(zebra.name, zebra.age, zebra.is_hungry)
-print(giraffe.name, giraffe.age, giraffe.is_hungry)
-print(panda.name, panda.age, panda.is_hungry)
+print(zebra.name, zebra.age, zebra.is_alive)
+print(giraffe.name, giraffe.age, giraffe.is_alive)
+print(panda.name, panda.age, panda.is_alive)
