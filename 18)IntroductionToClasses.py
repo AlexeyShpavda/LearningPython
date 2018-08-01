d467b22907d7f896917d2c68f0452b12e945868a
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-08-01 10:38:55 +0300

    Inheritance

--- 18)IntroductionToClasses.py
+++ 18)IntroductionToClasses.py
@@ -208,6 +208,26 @@ print("### Class It Up ###")
 #             return False
 
 print("### Instantiate an Object ###")
+# class Triangle(object):
+#     number_of_sides = 3
+#
+#     def __init__(self, angle1, angle2, angle3):
+#         self.angle1 = angle1
+#         self.angle2 = angle2
+#         self.angle3 = angle3
+#
+#     def check_angles(self):
+#         if (self.angle1 + self.angle2 + self.angle3) == 180:
+#             return True
+#         else:
+#             return False
+#
+# my_triangle = Triangle(30, 60, 90)
+#
+# print(my_triangle.number_of_sides)
+# print(my_triangle.check_angles())
+
+print("### Inheritance ###")
 class Triangle(object):
     number_of_sides = 3
 
@@ -222,7 +242,10 @@ class Triangle(object):
         else:
             return False
 
-my_triangle = Triangle(30, 60, 90)
+class Equilateral(Triangle):
+    angle = 60
 
-print(my_triangle.number_of_sides)
-print(my_triangle.check_angles())
+    def __init__(self):
+        self.angle1 = self.angle
+        self.angle2 = self.angle
+        self.angle3 = self.angle
