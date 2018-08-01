789b9bc9c92b61e8ad284602139cf05e759a668b
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-08-01 16:03:56 +0300

    Inheritance

--- 19)Classes.py
+++ 19)Classes.py
@@ -53,6 +53,27 @@ print("### Creating class methods ###")
 # my_car.display_car()
 
 print("### Modifying member variables ###")
+# class Car(object):
+#     condition = "new"
+#
+#     def __init__(self, model, color, mpg):
+#         self.model = model
+#         self.color = color
+#         self.mpg = mpg
+#
+#     def display_car(self):
+#         print("This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg)))
+#
+#     def drive_car(self):
+#         self.condition = "used"
+#
+# my_car = Car("DeLorean", "silver", 88)
+#
+# print(my_car.condition)
+# my_car.drive_car()
+# print(my_car.condition)
+
+print("### Inheritance ###")
 class Car(object):
     condition = "new"
 
@@ -67,8 +88,12 @@ class Car(object):
     def drive_car(self):
         self.condition = "used"
 
-my_car = Car("DeLorean", "silver", 88)
 
-print(my_car.condition)
-my_car.drive_car()
-print(my_car.condition)
+class ElectricCar(Car):
+    def __init__(self, model, color, mpg, battery_type):
+        self.model = model
+        self.color = color
+        self.mpg = mpg
+        self.battery_type = battery_type
+
+my_car = ElectricCar("DeLorean", "silver", 88, "molten salt")
