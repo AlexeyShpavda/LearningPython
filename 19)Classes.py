cea5bb7ff041f45d404995171fdb360f40d1f47c
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-08-01 15:44:19 +0300

    Class member variables

--- 19)Classes.py
+++ 19)Classes.py
@@ -1,5 +1,13 @@
 print("### Create an instance of a class ###")
+# class Car(object):
+#   pass
+#
+# my_car = Car()
+
+print("### Class member variables ###")
 class Car(object):
-  pass
+  condition = "new"
 
 my_car = Car()
+
+print(my_car.condition)
