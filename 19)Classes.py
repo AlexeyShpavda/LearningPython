3c0080807c529eabc19b69277a6035544733a1a7
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-08-01 15:48:26 +0300

    Initializing a class

--- 19)Classes.py
+++ 19)Classes.py
@@ -5,9 +5,19 @@ print("### Create an instance of a class ###")
 # my_car = Car()
 
 print("### Class member variables ###")
+# class Car(object):
+#   condition = "new"
+#
+# my_car = Car()
+#
+# print(my_car.condition)
+
+print("### Initializing a class ###")
 class Car(object):
   condition = "new"
+  def __init__(self, model, color, mpg):
+    self.model = model
+    self.color = color
+    self.mpg = mpg
 
-my_car = Car()
-
-print(my_car.condition)
+my_car = Car("DeLorean", "silver", 88)
