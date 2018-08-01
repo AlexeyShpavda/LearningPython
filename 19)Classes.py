afd6479414f9e5d880437462e18d14d9ec345e8b
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-08-01 16:11:44 +0300

    Building useful classes

--- 19)Classes.py
+++ 19)Classes.py
@@ -131,3 +131,16 @@ my_car = ElectricCar("DeLorean", "silver", 88, "molten salt")
 print(my_car.condition)
 my_car.drive_car()
 print(my_car.condition)
+
+print("### Building useful classes ###")
+class Point3D(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return "(%d, %d, %d)" % (self.x, self.y, self.z)
+
+my_point = Point3D(1, 2, 3)
+print(my_point)
