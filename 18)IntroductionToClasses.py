0bf62c69401510bbb9da6bc8b9a400ab1b3c2c79
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-08-01 10:21:09 +0300

    Warning: Here Be Dragons

--- 18)IntroductionToClasses.py
+++ 18)IntroductionToClasses.py
@@ -118,3 +118,21 @@ class ShoppingCart(object):
 
 my_cart = ShoppingCart("Eric")
 my_cart.add_item("Ukelele", 10)
+
+print("### Warning: Here Be Dragons ###")
+class Customer(object):
+    """Produces objects that represent customers."""
+    def __init__(self, customer_id):
+        self.customer_id = customer_id
+
+    def display_cart(self):
+        print("I'm a string that stands in for the contents of your shopping cart!")
+
+class ReturningCustomer(Customer):
+    """For customers of the repeat variety."""
+    def display_order_history(self):
+        print("I'm a string that stands in for your order history!")
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
