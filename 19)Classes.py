2f583975ed21f33bb480c0f71173471261a2893a
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-08-01 16:08:03 +0300

    Overriding methods

--- 19)Classes.py
+++ 19)Classes.py
@@ -74,6 +74,31 @@ print("### Modifying member variables ###")
 # print(my_car.condition)
 
 print("### Inheritance ###")
+# class Car(object):
+#     condition = "new"
+#
+#     def __init__(self, model, color, mpg):
+#         self.model = model
+#         self.color = color
+#         self.mpg = mpg
+#
+#     def display_car(self):
+#         print("This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg)))
+#
+#     def drive_car(self):
+#         self.condition = "used"
+#
+#
+# class ElectricCar(Car):
+#     def __init__(self, model, color, mpg, battery_type):
+#         self.model = model
+#         self.color = color
+#         self.mpg = mpg
+#         self.battery_type = battery_type
+#
+# my_car = ElectricCar("DeLorean", "silver", 88, "molten salt")
+
+print("### Overriding methods ###")
 class Car(object):
     condition = "new"
 
@@ -83,7 +108,8 @@ class Car(object):
         self.mpg = mpg
 
     def display_car(self):
-        print("This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg)))
+        print
+        "This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg))
 
     def drive_car(self):
         self.condition = "used"
@@ -96,4 +122,12 @@ class ElectricCar(Car):
         self.mpg = mpg
         self.battery_type = battery_type
 
+    def drive_car(self):
+        self.condition = "like new"
+
+
 my_car = ElectricCar("DeLorean", "silver", 88, "molten salt")
+
+print(my_car.condition)
+my_car.drive_car()
+print(my_car.condition)
